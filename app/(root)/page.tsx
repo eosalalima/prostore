cd040786b32e34b1c679d4c6689e15892225f2c8
Eric O. Salalima <eosalalima@gmail.com>
2025-11-13 04:40:00 +0000

    Setup Loading and Not Found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,3 @@
-// export const metadata = {
-//     title: "Home",
-// };
 
 const Homepage = () => {
     return <>Prostore</>;
---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const NotFoundPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <Image src="/images/logo.svg" alt={`${APP_NAME}`} width={48} height={48} priority={true} />
+
+            <div className="p-6 rounded-lg shadow-md text-center">
+                <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+                <p className="text-destructive">Sorry, the page you are looking for does not exist.</p>
+                <Button className="mt-4 ml-2" variant="outline" onClick={() => (window.location.href = '/')}>
+                    Back to Home
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
